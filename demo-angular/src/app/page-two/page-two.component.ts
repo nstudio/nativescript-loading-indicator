34b394f964b275cf821cd41553879fb728d90ca8
Alex Miller <codex.nz@gmail.com>
2020-02-25 16:07:30 +1300

    test(new demo) Added Angular demo
    This demo contains a series of tests to test odd behaviour found when consuming this plugin.

---
+++ demo-angular/src/app/page-two/page-two.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'page-two-page',
+    templateUrl: 'page-two.component.html'
+})
+export class PageTwoComponent {}
